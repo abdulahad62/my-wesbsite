caaf2fc44c81a94c8da9494f5cabd6e87ae8a1b3
abdulahad62 <72248442+abdulahad62@users.noreply.github.com>
2021-10-01 10:22:40 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,50 @@
+
 $(function () {
   $("#datepicker").datepicker({
     dateFormat: "dd-mm-yy",
     duration: "fast",
   });
 });
+
+$(function () {
+  $("#datepicker-to").datepicker({
+    dateFormat: "dd-mm-yy",
+    duration: "fast",
+  });
+});
+(function () {
+  "use strict";
+
+  $(".input-file").each(function () {
+    var $input = $(this),
+      $label = $input.next(".js-labelFile"),
+      labelVal = $label.html();
+
+    $input.on("change", function (element) {
+      var fileName = "";
+      if (element.target.value)
+        fileName = element.target.value.split("\\").pop();
+      fileName
+        ? $label.addClass("has-file").find(".js-fileName").html(fileName)
+        : $label.removeClass("has-file").html(labelVal);
+    });
+  });
+})();
+(function () {
+  "use strict";
+
+  $(".input-file-csv").each(function () {
+    var $input = $(this),
+      $label = $input.next(".js-labelFile"),
+      labelVal = $label.html();
+
+    $input.on("change", function (element) {
+      var fileName = "";
+      if (element.target.value)
+        fileName = element.target.value.split("\\").pop();
+      fileName
+        ? $label.addClass("has-file").find(".js-fileName").html(fileName)
+        : $label.removeClass("has-file").html(labelVal);
+    });
+  });
+})();
